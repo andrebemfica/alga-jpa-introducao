0712a2c0143353a0d0b421170fe6b365a7675af3
andrebemfica <andrecbemfica@gmail.com>
2023-04-02 13:12:45 -0300

    primeiro commit jpa introdução

---
+++ src/main/java/com/bemfis/algajpaintroducao/AlgaJpaIntroducaoApplication.java
@@ -0,0 +1,13 @@
+package com.bemfis.algajpaintroducao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgaJpaIntroducaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgaJpaIntroducaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bemfis/algajpaintroducao/AlgaJpaIntroducaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bemfis.algajpaintroducao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgaJpaIntroducaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
